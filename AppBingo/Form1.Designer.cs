480d000cdda46e37506e36d67377080221c46bf4
Henrique87645 <125613025+Henrique87645@users.noreply.github.com>
2025-05-27 09:54:21 -0300

    Add files via upload

---
+++ AppBingo/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AppBingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPremio = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPremio
+            // 
+            this.txtPremio.Location = new System.Drawing.Point(12, 45);
+            this.txtPremio.Name = "txtPremio";
+            this.txtPremio.Size = new System.Drawing.Size(146, 22);
+            this.txtPremio.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(176, 45);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(122, 23);
+            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.Text = "SALVAR";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Prêmio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtPremio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPremio;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AppBingo/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppBingo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string SoNumero(string Texto)
+        {
+            string resultado = "";
+            for (int i = 0; i < Texto.Length; i++)
+            {
+                if (char.IsDigit(Texto[i]))
+                {
+                    resultado += Texto[i];
+                }
+            }
+
+            //Retorno o resultado
+            return resultado;
+        }
+
+        bool ArquivoExiste(string caminho)
+        {
+            //verifica se o arquivo existe, o nome do arquivo sera o CPF, 
+            //portanto se já existir, o usuário já possue cadastro
+            return File.Exists(caminho);
+        }
+
+        void GravarArquivo(string caminho, string conteudo)
+        {
+            //grava o conteudo no arquivo
+            //se o arquivo não existir, ele será criado
+            string pasta = Path.GetDirectoryName(caminho);
+
+            if (!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+            File.WriteAllText(caminho, conteudo);
+        }
+
+        string GetDirArquivo(string nomePasta, string nomeArquivo)
+        {
+            //O arquivo será montado: Diretório Raiz do Executavel
+            // nome da pasta: será o tipo de cadastro
+            // nome do arquivo: será o CPF
+
+            string raizExe = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.Combine(raizExe, nomePasta, nomeArquivo + ".txt");
+        }
+
+        string GetCadastro()
+        {
+            //Iremos concatenar os dados do cadastro para gerear o conteudo do arquivo
+            string cadastro = "Nome do Prêmio: " + txtPremio.Text + Environment.NewLine +
+                            "Horário de início: " + DateTime.Now.ToShortTimeString();
+
+            return cadastro;
+        }
+
+        // Método para salvar o cadastro
+        void Salvar()
+        {
+            string date = DateTime.Now.ToShortDateString();
+            string time = DateTime.Now.ToShortTimeString();
+
+            string caminhoCompleto = GetDirArquivo("Bingo", ("Bingo_" + SoNumero(date) + "_" + SoNumero(time)));
+
+            //verifica se o arquivo já existe
+            if (ArquivoExiste(caminhoCompleto))
+            {
+                //se o arquivo já existe, o usuário já possui cadastro
+                MessageBox.Show("Usuário já cadastrado", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                //gravar o arquivo
+                GravarArquivo(caminhoCompleto, GetCadastro());
+
+                //se o arquivo foi gravado com sucesso, exibe uma mensagem de sucesso
+                MessageBox.Show("Registro salvo com sucesso!" + Environment.NewLine + Environment.NewLine +
+                    "Salvo em :" + caminhoCompleto, "Informção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar o cadastro." + Environment.NewLine + "Erro original: " + ex.Message,
+                    "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Salvar();
+        }
+    }
+}
